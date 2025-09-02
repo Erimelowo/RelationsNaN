c1dd9b8230c49d74fab01fd6e8657727ce9669a0
Erimel <marioluigivideo@gmail.com>
2025-09-02 13:23:42 -0400

    Fini

---
+++ Models/Purchase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace RelationsNaN.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        [ValidateNever]
+        public List<GamePurchase> GamePurchases { get; set; }
+    }
+}
